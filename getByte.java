b6dda2687702e20bb99dc0724cd1c2fc680179b2
Serkebaeva <aizada.serkebaeva.lt@gmail.com>
2024-10-07 09:42:46 +0200

    getBytes practise

---
+++ getByte.java
@@ -0,0 +1,18 @@
+
+public class getByte {
+
+    public static void main(String[] args) {
+        String c = "Hello I am practicing Java";
+        if (c.startsWith("H")) {
+            c = c.trim();
+            c = c.replaceAll("am", "was");
+            c = c.substring(3, 5);
+            byte[] b = c.getBytes();
+            for (int i = 0; i < b.length; i++) {
+                System.out.println(b[i]);
+            }
+        }
+        System.out.println(c);
+
+    }
+}
